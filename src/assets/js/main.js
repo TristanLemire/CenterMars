054ab191cf3f329dbce135c54b887146a9e1e55c
Julian Romana <you@example.com>
2019-04-16 20:09:47 +0200

    rip scroll

--- src/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-let cursor = document.querySelector("#cursor");
-window.addEventListener("scroll", () => {
-  let y = window.pageYOffset;
-  var height = document.height !== undefined ? document.height : document.body.offsetHeight;
-  let calc = 1 - (y * 2) / height;
-  let doit = 100 * (1 - calc);
-  cursor.style.top = doit + "%";
-});
