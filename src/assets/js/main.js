1ed773b3bd3706d07c044e80158744bb95e320c2
Julian Romana <you@example.com>
2019-04-17 11:21:21 +0200

    header colore when entering in white section

---
+++ src/assets/js/main.js
@@ -0,0 +1,13 @@
+let header = document.querySelector("header");
+let headerButton = document.querySelector("header li:nth-child(4)");
+console.log(headerButton);
+window.addEventListener("scroll", () => {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset > 754) {
+    header.classList.add("is-visible");
+    headerButton.classList.add("is-white");
+  } else {
+    header.classList.remove("is-visible");
+    headerButton.classList.remove("is-white");
+  }
+});
