5ff55f3c2827427c0d2aab75ced572d02d12290f
Julian Romana <you@example.com>
2019-04-18 20:13:46 +0200

    animation h2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,29 +14,24 @@ window.addEventListener("scroll", () => {
     h2.classList.add("is-translating");
   }
 });
-<<<<<<< HEAD
 import Rellax from "rellax";
 var rellax = new Rellax(".rellax");
-=======
-import Rellax from 'rellax';
-var rellax = new Rellax('.rellax');
 
-let caseElementsTest = document.querySelectorAll('.is-hidden');
+let caseElementsTest = document.querySelectorAll(".is-hidden");
 console.log(caseElementsTest);
 let elementsArray = [];
-for ( let i = 1; i < caseElementsTest.length + 1; i++ ) {
-    let caseElement = caseElementsTest[i];
-    i < 10 ? elementsArray.push(`element-0${i}`) : elementsArray.push(`element-${i}`);
+for (let i = 1; i < caseElementsTest.length + 1; i++) {
+  let caseElement = caseElementsTest[i];
+  i < 10 ? elementsArray.push(`element-0${i}`) : elementsArray.push(`element-${i}`);
 }
 
-elementsArray.forEach( ( id ) => {
-let element = document.getElementById( id );
-let waypoint = new Waypoint({
+elementsArray.forEach(id => {
+  let element = document.getElementById(id);
+  let waypoint = new Waypoint({
     element: element,
-    handler: function( direction ) {
-    element.classList.add( 'is-visible2' );
+    handler: function(direction) {
+      element.classList.add("is-visible2");
     },
-    offset: '70%'
+    offset: "70%"
+  });
 });
-});
->>>>>>> cdb932fc25a73a3b45323b24092a237784b1180f
