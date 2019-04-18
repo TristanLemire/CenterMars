cdb932fc25a73a3b45323b24092a237784b1180f
TristanLemire <tristan.lemire.sio@gmail.com>
2019-04-18 16:56:50 +0200

    waypoint

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -13,3 +13,22 @@ window.addEventListener("scroll", () => {
 });
 import Rellax from 'rellax';
 var rellax = new Rellax('.rellax');
+
+let caseElementsTest = document.querySelectorAll('.is-hidden');
+console.log(caseElementsTest);
+let elementsArray = [];
+for ( let i = 1; i < caseElementsTest.length + 1; i++ ) {
+    let caseElement = caseElementsTest[i];
+    i < 10 ? elementsArray.push(`element-0${i}`) : elementsArray.push(`element-${i}`);
+}
+
+elementsArray.forEach( ( id ) => {
+let element = document.getElementById( id );
+let waypoint = new Waypoint({
+    element: element,
+    handler: function( direction ) {
+    element.classList.add( 'is-visible2' );
+    },
+    offset: '70%'
+});
+});
